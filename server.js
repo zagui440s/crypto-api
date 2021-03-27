08bcc6d2cb78267debb5c6db2054d1865e2533e6
Zeus_Aguilar <zaguilar2142@gmail.com>
2021-03-27 16:52:36 -0500

    app update

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.get('/api/currency/:contsCoin', (request, response) => {
 
 
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
